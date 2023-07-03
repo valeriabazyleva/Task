e97bd49c845926bac3a242175f5adcf6a7f1d28e
Valeria Bazyleva <valeryiabazyleva@gmail.com>
2023-07-03 20:09:07 +0300

    The solution of the problem

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Авиаперевозчики предлагают различные бонусные программы, начисляющие бесплатные мили за перелёты.
+// Формула расчёта следующая: за каждые 20 рублей, потраченные на билет, начисляется 1 миля. Дробные мили не начисляются.
+//Например, при стоимости билета в 13 676 рублей должно начислиться 683 бонусов.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int ticketPrice = 13676; // стоимость билета
+        int rubles = 20; // сумма от которой начисляется 1 миля
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int miles = (int)(ticketPrice / rubles);
+
+        System.out.println(miles + " начисленных бонусов");
+
+
     }
 }
